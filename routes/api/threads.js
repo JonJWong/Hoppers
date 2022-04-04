deda5fd54b13200ae2dbce79106ad6eb7160f377
Kevin Chen <61332776+KevinCh28@users.noreply.github.com>
2022-04-04 19:21:15 -0400

    Thread routes (#12)
    * add thread routes
    
    * add user to new thread in post
    
    * refactor thread routes
    
    * add route to index threads of an event, create a thread, delete a thread
    
    * add get thread by id

---
+++ routes/api/threads.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
+
+const Thread = require('../../models/Thread');
+const validateThreadInput = require('../../validation/threads');
+
+router.get('/event/:event_id', (req, res) => {
+  Thread.find({ event: req.params.event_id })
+    .then(thread => res.json(thread))
+    .catch(err => res.status(404).json({ nothreadfound: 'No thread found from that event' }))
+});
+
+router.get('/:id', (req, res) => {
+  Thread.findById(req.params.id)
+    .then(thread => res.json(thread))
+    .catch(err => res.status(404).json({ nothreadfound: 'No thread found with that ID' }))
+});
+
+router.post('/',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validateThreadInput(req.body);
+
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }
+
+    const newThread = new Thread({
+      name: req.body.name,
+      event: req.body.eventId,
+    })
+
+    newThread.save().then(thread => res.json(thread));
+  });
+
+router.delete('/:id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+
+    Thread.findById(req.params.id)
+      .then(thread => thread.delete())
+      .then(res.json("Thread deleted"))
+  })
+
+module.exports = router;
+
+
