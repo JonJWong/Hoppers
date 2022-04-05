6ede879d4948dd82e94b9f59fa74a04d861cbf9e
JonJWong <wongjonathan01@gmail.com>
2022-04-05 18:49:25 -0400

    Initial map setup

---
+++ frontend/src/components/map/index_map.jsx
@@ -0,0 +1,269 @@
+import React from "react";
+
+const MAP_OPTIONS = {
+  center: { lat: 37.7758, lng: -122.435 }, // this is SF
+  zoom: 13,
+  streetViewControl: false,
+  mapTypeControl: false,
+  keyboardShortcuts: false,
+  backgroundColor: 'none',
+  fullscreenControl: false,
+};
+
+const STYLES = {
+  default: [
+    {
+      featureType: "administrative",
+      stylers: [{visibility: "off"}]
+    },
+    {
+      featureType: "administrative.land_parcel",
+      elementType: "labels",
+      stylers: [{visibility: "off"}]
+    },
+    {
+      featureType: "poi",
+      elementType: "labels.text",
+      stylers: [{visibility: "off"}]
+    },
+    {
+      featureType: "poi.business",
+      stylers: [{visibility: "off"}]
+    },
+    {
+      featureType: "poi.business",
+      elementType: "geometry",
+      stylers: [{visibility: "off"}]
+    },
+    {
+      featureType: "poi.government",
+      stylers: [{visibility: "off"}]
+    },
+    {
+      featureType: "poi.medical",
+      stylers: [{visibility: "off"}]
+    },
+    {
+      featureType: "poi.park",
+      elementType: "labels.text",
+      stylers: [{visibility: "off"}]
+    },
+    {
+      featureType: "poi.place_of_worship",
+      stylers: [{visibility: "off"}]
+    },
+    {
+      featureType: "poi.school",
+      stylers: [{visibility: "off"}]
+    },
+    {
+      featureType: "poi.sports_complex",
+      stylers: [{visibility: "off"}]
+    },
+    {
+      featureType: "road.local",
+      elementType: "labels",
+      stylers: [{visibility: "off"}]
+    },
+    {
+      featureType: "transit",
+      stylers: [{visibility: "off"}]
+    }
+  ],
+  dark: [
+    { elementType: "geometry", stylers: [{ color: "#242f3e" }] },
+    { elementType: "labels.text.stroke", stylers: [{ color: "#242f3e" }] },
+    { elementType: "labels.text.fill", stylers: [{ color: "#746855" }] },
+    {
+      featureType: "administrative.locality",
+      elementType: "labels.text.fill",
+      stylers: [{ color: "#d59563" }],
+    },
+    {
+      featureType: "poi",
+      elementType: "labels.text.fill",
+      stylers: [{ color: "#d59563" }],
+    },
+    {
+      featureType: "poi.park",
+      elementType: "geometry",
+      stylers: [{ color: "#263c3f" }],
+    },
+    {
+      featureType: "poi.park",
+      elementType: "labels.text.fill",
+      stylers: [{ color: "#6b9a76" }],
+    },
+    {
+      featureType: "road",
+      elementType: "geometry",
+      stylers: [{ color: "#38414e" }],
+    },
+    {
+      featureType: "road",
+      elementType: "geometry.stroke",
+      stylers: [{ color: "#212a37" }],
+    },
+    {
+      featureType: "road",
+      elementType: "labels.text.fill",
+      stylers: [{ color: "#9ca5b3" }],
+    },
+    {
+      featureType: "road.highway",
+      elementType: "geometry",
+      stylers: [{ color: "#746855" }],
+    },
+    {
+      featureType: "road.highway",
+      elementType: "geometry.stroke",
+      stylers: [{ color: "#1f2835" }],
+    },
+    {
+      featureType: "road.highway",
+      elementType: "labels.text.fill",
+      stylers: [{ color: "#f3d19c" }],
+    },
+    {
+      featureType: "transit",
+      elementType: "geometry",
+      stylers: [{ color: "#2f3948" }],
+    },
+    {
+      featureType: "transit.station",
+      elementType: "labels.text.fill",
+      stylers: [{ color: "#d59563" }],
+    },
+    {
+      featureType: "water",
+      elementType: "geometry",
+      stylers: [{ color: "#17263c" }],
+    },
+    {
+      featureType: "water",
+      elementType: "labels.text.fill",
+      stylers: [{ color: "#515c6d" }],
+    },
+    {
+      featureType: "water",
+      elementType: "labels.text.stroke",
+      stylers: [{ color: "#17263c" }],
+    },
+    {
+      featureType: "administrative",
+      stylers: [{visibility: "off"}]
+    },
+    {
+      featureType: "administrative.land_parcel",
+      elementType: "labels",
+      stylers: [{visibility: "off"}]
+    },
+    {
+      featureType: "poi",
+      elementType: "labels.text",
+      stylers: [{visibility: "off"}]
+    },
+    {
+      featureType: "poi.business",
+      stylers: [{visibility: "off"}]
+    },
+    {
+      featureType: "poi.business",
+      elementType: "geometry",
+      stylers: [{visibility: "off"}]
+    },
+    {
+      featureType: "poi.government",
+      stylers: [{visibility: "off"}]
+    },
+    {
+      featureType: "poi.medical",
+      stylers: [{visibility: "off"}]
+    },
+    {
+      featureType: "poi.park",
+      elementType: "labels.text",
+      stylers: [{visibility: "off"}]
+    },
+    {
+      featureType: "poi.place_of_worship",
+      stylers: [{visibility: "off"}]
+    },
+    {
+      featureType: "poi.school",
+      stylers: [{visibility: "off"}]
+    },
+    {
+      featureType: "poi.sports_complex",
+      stylers: [{visibility: "off"}]
+    },
+    {
+      featureType: "road.local",
+      elementType: "labels",
+      stylers: [{visibility: "off"}]
+    },
+    {
+      featureType: "transit",
+      stylers: [{visibility: "off"}]
+    }
+  ],
+  hiding: [
+    {
+      featureType: "administrative",
+      elementType: "labels",
+      stylers: [{ visibility: "off" }]
+    },
+    {
+      featureType: "poi.business",
+      stylers: [{ visibility: "off" }],
+    },
+    {
+      featureType: "transit",
+      elementType: "labels.icon",
+      stylers: [{ visibility: "off" }],
+    },
+  ],
+};
+
+
+class IndexMap extends React.Component{
+  constructor(props){
+    super(props)
+
+    this.markers = {};
+  }
+
+  placeMarker(location) {
+    // create marker at the location specified
+    const marker = new window.google.maps.Marker({
+      position: location,
+      map: this.map
+    })
+  }
+
+  componentDidMount() {
+    // we want to configure the map options to be able to display all of the 
+    // points of interest for an event. We can get the center (average)
+    // and then configure the radius to display all of the point within the bounds
+    this.map = new window.google.maps.Map(this.mapNode, MAP_OPTIONS);
+
+    // get time of day and set a styles var accordingly
+    const hour = new Date().getHours();
+    let styles;
+    if (hour < 7 || hour > 17) {
+      styles = STYLES["dark"]
+    } else {
+      styles = STYLES["default"]
+    }
+    // apply styles by time of day
+    this.map.setOptions({ styles: styles});
+  }
+
+  render(){
+    return(
+      <div id="map-container" ref={ map => this.mapNode = map }></div>
+    )
+  }
+};
+
+export default IndexMap;
