4877b625bedc8e37d65321f568ab0476bc6e63aa
Kevin Chen <kevinch28@gmail.com>
2022-04-05 14:53:15 -0400

    add create edit delete comment and update create thread to be in events

--- routes/api/threads.js
+++ routes/api/threads.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 const Thread = require('../../models/Thread');
+const Event = require('../../models/Event');
 const validateThreadInput = require('../../validation/threads');
+const validateCommentInput = require('../../validation/comments');
 
 router.get('/event/:event_id', (req, res) => {
   Thread.find({ event: req.params.event_id })
@@ -20,17 +22,18 @@ router.post('/',
   passport.authenticate('jwt', { session: false }),
   (req, res) => {
     const { errors, isValid } = validateThreadInput(req.body);
-
-    if (!isValid) {
-      return res.status(400).json(errors);
-    }
+    if (!isValid) { return res.status(400).json(errors) }
 
     const newThread = new Thread({
       name: req.body.name,
       event: req.body.eventId,
     })
 
-    newThread.save().then(thread => res.json(thread));
+    Event.findById(req.body.eventId)
+      .then(currentEvent => {
+        currentEvent.threads.push(newThread)
+        currentEvent.save().then(event => res.json(event))
+      })
   });
 
 router.delete('/:id',
@@ -48,12 +51,59 @@ router.patch('/:id',
     
     Thread.findById(req.params.id)
       .then(thread => {
-        thread.name = req.body.name
+        const { errors, isValid } = validateThreadInput(req.body);
+        if (!isValid) { return res.status(400).json(errors) }
 
+        thread.name = req.body.name
         thread.save().then(thread => res.json(thread));
       })
       .catch(err => res.status(404).json({ nothreadfound: 'No thread found with that ID' }))
 })
+
+router.post('/:id/comments',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+
+    Thread.findById(req.params.id)
+      .then(thread => {
+        const { errors, isValid } = validateCommentInput(req.body);
+        if (!isValid) {return res.status(400).json(errors)}
+
+        thread.comments.push(req.body)
+        thread.save().then(thread => res.json(thread));
+      })
+      .catch(err => res.status(404).json({ nothreadfound: 'No thread found with that ID' }))
+  })
+
+router.patch('/:id/comments/:comment_id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+
+    Thread.findById(req.params.id)
+      .then(thread => {
+        const { errors, isValid } = validateCommentInput(req.body);
+        if (!isValid) { return res.status(400).json(errors) }
+
+        let commentIndex = thread.comments.findIndex(comment => { return comment.id === req.params.comment_id})
+        thread.comments[commentIndex] = req.body;
+        thread.save().then(thread => res.json(thread));
+      })
+      .catch(err => res.status(404).json({ nothreadfound: 'No thread found with that ID' }))
+  })
+
+router.delete('/:id/comments/:comment_id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+
+    Thread.findById(req.params.id)
+      .then(thread => {
+
+        let commentIndex = thread.comments.findIndex(comment => { return comment.id === req.params.comment_id })
+        thread.comments.splice(commentIndex, 1);
+        thread.save().then(thread => res.json(thread));
+      })
+      .catch(err => res.status(404).json({ nothreadfound: 'No thread found with that ID' }))
+  })
 
 module.exports = router;
 
