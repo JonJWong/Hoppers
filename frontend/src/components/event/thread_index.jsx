f2769486b094eaf032d2aa06ff0df372afc7f588
Nick Pietrow <njpietrow@gmail.com>
2022-04-05 16:34:43 -0700

    update event show page load dynamics

--- frontend/src/components/event/thread_index.jsx
+++ frontend/src/components/event/thread_index.jsx
@@ -2,26 +2,16 @@ import React from "react";
 import ThreadIndexItem from "./thread_index_item";
 
 class ThreadIndex extends React.Component{
-
-  renderThreads(){
-    const {threads} = this.props;
-    if ((threads.length === 0) || (typeof threads[0] === "string")){
-      return null
-    } else {
-      return (
-        threads.map((thread,idx) => (
-          <ThreadIndexItem key={`${thread._id}+${idx}`} thread={thread} />
-        ))
-      )
-    }
-  }
-
   render(){
+    const {threads} = this.props;
+    if(!threads) return null
     return(
       <div className="thread-index">
         <h3>Conversation Threads</h3>
         <div className="thread-list-container">
-          {this.renderThreads()}
+          {threads.map((thread,idx) => (
+            <ThreadIndexItem key={`${thread._id}+${idx}`} thread={thread} />
+          ))}
         </div>
       </div>
     );
