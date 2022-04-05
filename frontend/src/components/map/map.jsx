89ad1770b270048cb4476cbbfa6b99f7a940da8b
Nick Pietrow <njpietrow@gmail.com>
2022-04-04 18:28:40 -0700

    update map component by attaching to window

--- frontend/src/components/map/map.jsx
+++ frontend/src/components/map/map.jsx
@@ -7,9 +7,8 @@ class Map extends React.Component{
       center: { lat: 37.7758, lng: -122.435 }, // this is SF
       zoom: 13
     };
-
     // wrap this.mapNode in a Google Map
-    // this.map = new google.maps.Map(this.mapNode, mapOptions);
+    this.map = new window.google.maps.Map(this.mapNode, mapOptions);
   }
 
   render(){
