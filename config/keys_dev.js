3c371505b951c4941b6f7141160d313e9dd90715
CodyDegraffeNiles <79245580+CodyDegraffeNiles@users.noreply.github.com>
2022-04-05 17:49:56 -0400

    Delete keys_dev.js

--- config/keys_dev.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  mongoURI: 'mongodb+srv://njpietrow:zUztbjD6hfAaY0a3@hoppers.fz1f6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-  secretOrKey: 'TTquMhuOV0u2PwaVyMpZsd747EF8QHQb',
-  mapsApiKey: 'AIzaSyAB8PljWcO7KMN-04RYz5t9VBF1TwoRQ00'
-}
