c52b4ac80746e90347fe9da19ca1262b29b5e808
Kevin Chen <kevinch28@gmail.com>
2022-04-05 18:42:00 -0400

    add username to comments

--- validation/comments.js
+++ validation/comments.js
@@ -6,10 +6,15 @@ module.exports = function validateCommentInput(data) {
 
   data.body = validText(data.body) ? data.body : '';
 
+  data.username = validText(data.username) ? data.username : '';
   // Validate body
   
   if (Validator.isEmpty(data.body)) {
     errors.body = 'Body field is required'; 
+  }
+
+  if (Validator.isEmpty(data.username)) {
+    errors.username = 'Username field is required';
   }
 
   return {
