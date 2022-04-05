6d9ea1e1d1faebe2e9b779b31f46f64e4f037f6d
Nick J Pietrow <njpietrow@gmail.com>
2022-04-05 16:36:27 -0700

    Show attendees (#38)
    * updates
    
    * update event show page load dynamics

--- frontend/src/components/event/attendee_index.jsx
+++ frontend/src/components/event/attendee_index.jsx
@@ -1,26 +1,19 @@
 import React from "react";
 
 class AttendeeIndex extends React.Component{
-  renderAttendees(){
-    const {attendees} = this.props;
-    if ((attendees.length === 0) || (typeof attendees[0] === "string")){
-      return null
-    } else {
-      return (
-        attendees.map(person => (
-          <div key={person.username}>
-            {person.username}
-          </div>
-        ))
-      )
-    }
-  }
-
   render(){
+    const {attendees} = this.props;
+    if(!attendees){
+      return null
+    }
     return(
       <div className="attendee-list">
         <h3>Attendee List</h3>
-        {this.renderAttendees()}
+          {attendees.map(person => (
+            <div key={person.username}>
+              {person.username}
+            </div>
+          ))}
       </div>
     )
   }
