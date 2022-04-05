ff8cb36541ab63bd025f4d911152515f591c6583
JonJWong <wongjonathan01@gmail.com>
2022-04-05 12:59:17 -0400

    Update nav to have modal functionality

--- frontend/src/components/nav/navbar.js
+++ frontend/src/components/nav/navbar.js
@@ -1,16 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
+import LoginFormContainer from "../session/login_form_container";
+import SignupFormContainer from "../session/signup_form_container";
 
 class NavBar extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      modalOpen: false,
+      currentForm: ""
+    }
+
     this.logoutUser = this.logoutUser.bind(this);
     this.getLinks = this.getLinks.bind(this);
+    this.openModal = this.openModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+    this.openForm = this.openForm.bind(this);
+    this.renderForm = this.renderForm.bind(this);
+    this.renderModal = this.renderModal.bind(this);
   }
 
   logoutUser(e) {
-      e.preventDefault();
-      this.props.logout();
+    e.preventDefault();
+    this.props.logout();
   }
 
   // Selectively render links dependent on whether the user is logged in
@@ -32,36 +44,77 @@ class NavBar extends React.Component {
       } else {
         return (
             <div id="nav-button-container">
-                <Link
-                  to={'/signup'}>
-                    <button
-                      id="nav-bar-signup">
-                      Sign Up
-                    </button>
-                </Link>
-                <Link
-                  to={'/login'}>
-                    <button
-                      id="nav-bar-login">
-                      Log In
-                    </button>
-                </Link>
+              <button
+                id="nav-bar-signup"
+                onClick={() => this.openForm("signup")}>
+                Sign Up
+              </button>
+              <button
+                id="nav-bar-login"
+                onClick={() => this.openForm("login")}>
+                Log In
+              </button>
             </div>
         );
       }
   }
 
-  render() {
+  openModal() {
+    this.setState({ modalOpen: true });
+  }
+
+  closeModal() {
+    this.setState({
+      modalOpen: false,
+      currentForm: ""
+    });
+  }
+
+  openForm(type) {
+    this.setState({
+      currentForm: type,
+      modalOpen: true
+    })
+  }
+
+  renderForm() {
+    const { currentForm } = this.state;
+    if (currentForm === "signup") return <SignupFormContainer />;
+    if (currentForm === "login") return <LoginFormContainer />;
+  }
+
+  renderModal() {
+    if (this.state.modalOpen) {
       return (
-        <div id="nav-bar-wrapper">
-          <div id="nav-bar-container">
-            <button id="nav-bar-burger">
-              <i className="fa-solid fa-bars"></i>
-            </button>
-            { this.getLinks() }
+        <div
+          id="nav-modal-left-wrapper">
+            <div
+              id="nav-modal-left-container">
+                {this.renderForm()}
+            </div>
+          <div 
+            id="nav-modal-left-block"
+            onClick={() => this.closeModal()}>
           </div>
         </div>
-      );
+      )
+    }
+  }
+
+  render() {
+    return (
+      <div id="nav-bar-wrapper">
+        <div id="nav-bar-container">
+          <button
+            id="nav-bar-burger"
+            onClick={() => this.openModal()}>
+            <i className="fa-solid fa-bars"></i>
+          </button>
+          { this.getLinks() }
+        </div>
+        { this.renderModal() }
+      </div>
+    );
   }
 }
 
