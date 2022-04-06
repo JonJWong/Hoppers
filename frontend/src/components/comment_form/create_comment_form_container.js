1e7848bc859c60879344f3cfb20ffdc417ed85b6
Nick J Pietrow <njpietrow@gmail.com>
2022-04-06 15:37:17 -0700

    Comment form (#59)
    * added comment form to the thread index
    
    * udpated styling on comment thread

--- frontend/src/components/comment_form/create_comment_form_container.js
+++ frontend/src/components/comment_form/create_comment_form_container.js
@@ -1,16 +1,17 @@
 import { connect } from "react-redux";
 import CommentForm from "./comment_form";
+import { createComment } from "../../actions/thread_actions";
 
-const mSTP = state => ({
-  post: {
-    title: "",
-    body: ""
+const mSTP = (state, ownProps) => ({
+  comment: {
+    body: "",
+    username: state.session.user.username,
   },
   formType: "Leave Comment"
 });
 
 const mDTP = dispatch => ({
-
+  action: (threadId, comment) => dispatch(createComment(threadId,comment)),
 });
 
 export default connect(mSTP,mDTP)(CommentForm);
--- frontend/src/components/event/comment_index.jsx
+++ frontend/src/components/event/comment_index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import CommentIndexItem from "./comment_index_item";
+import CreateCommentFormContainer from "../comment_form/create_comment_form_container";
 
 class CommentIndex extends React.Component{
   render(){
@@ -10,8 +11,15 @@ class CommentIndex extends React.Component{
     return(
       <div className="comment-index">
         {Object.values(comments).map(comment => (
-          <CommentIndexItem key={comment._id} comment={comment} />
+          <CommentIndexItem 
+            key={comment._id} 
+            comment={comment} 
+            threadId={this.props.threadId}
+            currentUserUsername={this.props.currentUserUsername}
+            deleteComment={this.props.deleteComment}
+          />
         ))}
+        <CreateCommentFormContainer threadId={this.props.threadId}/>
       </div>  
     )
   }
