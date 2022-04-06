b1f9fa9d5b7d302ce04f9e02ca5b1fe5bbd07f18
Kevin Chen <kevinch28@gmail.com>
2022-04-05 20:16:35 -0400

    add axios event request, event create update delete thunk actions, remove event reducer

--- frontend/src/util/event_api_util.js
+++ frontend/src/util/event_api_util.js
@@ -7,3 +7,15 @@ export const getEvents = () => {
 export const getEvent = (eventId) => {
   return axios.get(`/api/events/${eventId}`)
 };
+
+export const makeEvent = (data) => {
+  return axios.post('/api/events', data)
+};
+
+export const editEvent = (eventId, data) => {
+  return axios.patch(`/api/events/${eventId}`, data)
+}
+
+export const deleteEvent = (eventId) => {
+  return axios.delete(`/api/events/${eventId}`)
+};
