e762abc40bd9c434a266497ef5d0bd723845d6b0
Jonathan Wong <96433015+JonJWong@users.noreply.github.com>
2022-04-06 14:55:34 -0400

    Create own user events (#53)
    * Add user-section on event index
    
    * Add anchor tag to css reset
    
    * Fix overflow scroll
    
    * Fix typo in profile container
    
    * Update styling on profile
    
    * Add padding to profile container

--- frontend/src/components/profile/profile.jsx
+++ frontend/src/components/profile/profile.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import EventIndexItem from "../event_index/event_index_item";
 import { Link } from 'react-router-dom';
 
 class Profile extends React.Component {
@@ -8,14 +9,56 @@ class Profile extends React.Component {
     this.state = {
     }
   }
+
+  componentDidMount(){
+    this.props.fetchEvents();
+  }
+
+  renderEventBar() {
+    return (
+      <div id="profile-self-section">
+        <div id="profile-self-top">
+          <div id="profile-self-title">
+            Events you're hosting:
+          </div>
+          <div id="profile-self-button-container">
+            <Link to="events/create">
+              <button id="profile-self-create">Create New Event</button>
+            </Link>
+          </div>
+        </div>
+
+        {this.renderOwnEvents()}
+      </div>
+    )
+  }
+
+  renderOwnEvents() {
+    if(Object.values(this.props.allEvents).length === 0) {
+      return null
+    }
+
+    return (
+      <ul id="profile-self-event-list">
+        {this.props.allEvents?.map(event => {
+          if (event.owner === this.props.currentUser.id) {
+            return <EventIndexItem key={event._id} event={event} />
+          }
+        })}
+      </ul>
+    )
+  }
   
   render() {
     return (
       <div className='profile-container'>
         <div className='profile-content'>
-          <div>Hello {this.props.currentUser.username}</div>
-          <Link to="/events">To events page</Link>
+          <div id="profile-greeting">Hello, {this.props.currentUser.username}</div>
+          <Link
+            to="/events"
+            id="profile-link">To events page</Link>
         </div>
+        {this.renderEventBar()}
       </div>
     );
   }
