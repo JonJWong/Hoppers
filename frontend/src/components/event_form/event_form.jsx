166e8abc5d908f404ba3ab90d751f170e064c870
JonJWong <wongjonathan01@gmail.com>
2022-04-06 18:20:53 -0400

    Style form

--- frontend/src/components/event_form/event_form.jsx
+++ frontend/src/components/event_form/event_form.jsx
@@ -101,39 +101,51 @@ class EventForm extends React.Component{
 
   render(){  
     return( 
-    <div>
-      <h5>{this.props.formType}</h5>
-      <form onSubmit ={this.handleSubmit}>
+    <div id="create-form-wrapper">
+      <h5 id="create-form-header">{this.props.formType}</h5>
+      <form
+        id="create-form-container"
+        onSubmit ={this.handleSubmit}>
 
-        <span id="create-form-name">Name:</span>
-        <input
-          type = "text"
-          value = {this.state.name}
-          onChange = {this.update("name")}
-        />
+        <div id="create-form-fields">
+          <div id="create-form-name-wrapper">
+            <div id="create-form-name">Name</div>
+              <input
+                type="text"
+                value={this.state.name}
+                onChange={this.update("name")}
+                placeholder="Event Name"
+              />
+          </div>
+          
+          <div id="create-form-description-wrapper">
+            <div id="create-form-description">Description</div>
+              <textarea
+              value={this.state.description}
+              onChange={this.update("description")}
+              /> 
+          </div>
+          
+          <div id="create-form-start-wrapper">
+            <div id ="create-form-start-time">Start Time</div>
+              <input 
+                type="datetime-local"
+                value={this.state.startTime}
+                onChange={this.update("startTime")}
+              />
+          </div>
+          
+          <div id="create-form-start-date-wrapper">
+            <div id ="create-form-start-date">End Time</div>
+              <input 
+                type="datetime-local"
+                value={this.state.endTime}
+                onChange = {this.update("endTime")}
+              />
+          </div>
+        </div>
 
-        <span id="create-form-description">Description </span>
-          <input 
-          type = "text"
-          value = {this.state.description}
-          onChange = {this.update("description")}
-          /> 
-
-        <span id ="create-form-start-time"> Start Time </span>
-        <input 
-          type="datetime-local"
-          value = {this.state.startTime}
-          onChange = {this.update("startTime")}
-        />
-
-        <span id ="create-form-start-date"> End Time </span>
-        <input 
-          type="datetime-local"
-          value = {this.state.endTime}
-          onChange = {this.update("endTime")}
-        />
-
-        <FunctionalMap event={this.state} accept={this.accept} />
+      <FunctionalMap event={this.state} accept={this.accept} />
 
         <div id="poi-input-list">
           {this.renderPoiInputs()}
