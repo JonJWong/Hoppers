bd4bc62213b49bf6969902686580c0443e301b7f
Nick Pietrow <njpietrow@gmail.com>
2022-04-05 19:19:38 -0700

    update modal links

--- frontend/src/components/nav/nav_profile.jsx
+++ frontend/src/components/nav/nav_profile.jsx
@@ -5,21 +5,28 @@ class NavProfile extends React.Component{
   render(){
     const {user, modalLogout, closeModal} = this.props
     return(
-      <div className='modal-content-container'> 
-        <div className='modal-profile-container'>
-          <div className='circle select-none'>
-            {user.username[0]}
+      <div className='modal-content-container'>
+        <div className="modal-profile-wrapper">
+          <div className='modal-profile-container'>
+            <div className='circle select-none'>
+              {user.username[0]}
+            </div>
+            <div>
+              <h4>{user.username}</h4>
+              <Link to={'/profile'} onClick={closeModal}>My Profile</Link>
+            </div>
           </div>
-          <div>
-            <h4>{user.username}</h4>
-            <Link to={'/profile'} onClick={closeModal}>My Profile</Link>
+          <div className="modal-links">
+            <div className="modal-link">
+              <Link to={'/events'} onClick={closeModal}><i className="fa-solid fa-calendar-days"></i> Events</Link>
+            </div>
+            <button
+              id="nav-modal-signout"
+              onClick={modalLogout}>
+                Sign Out
+            </button>
           </div>
-        </div>
-        <button
-          id="nav-modal-signout"
-          onClick={modalLogout}>
-            Sign Out
-        </button>
+        </div> 
       </div>
     )
   }
