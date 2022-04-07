f17118a484a44821da4b0a646a20a12a52933e3d
Kevin Chen <kevinch28@gmail.com>
2022-04-06 23:10:52 -0400

    add errors handling for thread form

--- frontend/src/actions/event_actions.js
+++ frontend/src/actions/event_actions.js
@@ -3,6 +3,8 @@ import { getEvents, getEvent, makeEvent, editEvent, deleteEvent, addAttendee, re
 export const RECEIVE_EVENTS = "RECEIVE_EVENTS";
 export const RECEIVE_EVENT = "RECEIVE_EVENT";
 export const REMOVE_EVENT = "REMOVE_EVENT";
+export const RECEIVE_EVENT_ERRORS = "RECEIVE_EVENT_ERRORS";
+export const REMOVE_EVENT_ERRORS = "REMOVE_EVENT_ERRORS";
 
 export const receiveEvents = (events) => ({
   type: RECEIVE_EVENTS,
@@ -19,62 +21,65 @@ export const removeEvent = (eventId) => ({
   eventId
 });
 
+export const receiveEventErrors = (errors) => ({
+  type: RECEIVE_EVENT_ERRORS,
+  errors
+});
+
+export const removeEventErrors = (errors) => ({
+  type: REMOVE_EVENT_ERRORS
+});
+
 export const fetchEvents = () => dispatch => (
   getEvents()
     .then(events => dispatch(receiveEvents(events)))
-    .catch(err => console.log(err))
 );
 
 export const fetchEvent = (eventId) => dispatch => (
   getEvent(eventId)
     .then(event => dispatch(receiveEvent(event)))
-    .catch(err => console.log(err))
 );
 
 export const createEvent = (data) => dispatch => (
   makeEvent(data)
     .then(event => dispatch(receiveEvent(event)))
-    .catch(err => console.log(err))
+    .catch(err => dispatch(receiveEventErrors(err.response.data)))
 );
 
 export const updateEvent = (data) => dispatch => (
   editEvent(data)
     .then(event => dispatch(receiveEvent(event)))
-    .catch(err => console.log(err))
+    .catch(err => dispatch(receiveEventErrors(err.response.data)))
 );
 
 export const deleteCurrentEvent = (eventId) => dispatch => (
   deleteEvent(eventId)
     .then(() => dispatch(removeEvent(eventId)))
-    .catch(err => console.log(err))
 );
 
 export const addNewPoi = (eventId, data) => dispatch => (
   addPoi(eventId, data)
     .then(event => dispatch(receiveEvent(event)))
-    .catch(err => console.log(err))
+    .catch(err => dispatch(receiveEventErrors(err.response.data)))
 )
 
 export const updatePoi = (eventId, data) => dispatch => (
   editPoi(eventId, data)
     .then(event => dispatch(receiveEvent(event)))
-    .catch(err => console.log(err))
+    .catch(err => dispatch(receiveEventErrors(err.response.data)))
 )
 
 export const deletePoi = (eventId, poiId) => dispatch => (
   removePoi(eventId, poiId)
     .then(event => dispatch(receiveEvent(event)))
-    .catch(err => console.log(err))
 )
 
 export const addNewAttendee = (eventId, userId) => dispatch => (
   addAttendee(eventId, userId)
     .then(event => dispatch(receiveEvent(event)))
-    .catch(err => console.log(err))
 )
 
 export const deleteAttendee = (eventId, userId) => dispatch => (
   removeAttendee(eventId, userId)
     .then(event => dispatch(receiveEvent(event)))
-    .catch(err => console.log(err))
 )
