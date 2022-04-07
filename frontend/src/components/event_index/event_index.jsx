2fa27625de5d827e699e6d3a7371c9345503571d
Jonathan Wong <96433015+JonJWong@users.noreply.github.com>
2022-04-07 09:35:55 -0400

    Event index shadow (#71)
    * event index shadow
    
    * update styling
    
    * make index items be links with pointer
    
    * fix box formatting on index and profile pages
    
    Co-authored-by: Nick Pietrow <njpietrow@gmail.com>

--- frontend/src/components/event_index/event_index.jsx
+++ frontend/src/components/event_index/event_index.jsx
@@ -11,14 +11,10 @@ class EventIndex extends React.Component{
     return (
       <div id="index-self-section">
         <div id="index-self-top">
-          <div id="index-self-title">
+          {/* <div id="index-self-title">
             Your Events:
-          </div>
-          <div id="index-self-button-container">
-            <Link to="events/create">
-              <button id="index-self-create">Create New Event</button>
-            </Link>
-          </div>
+          </div> */}
+
         </div>
 
         {this.renderOwnEvents()}
@@ -39,6 +35,9 @@ class EventIndex extends React.Component{
               ? <EventIndexItem key={event._id} event={event} />
               : null
           })}
+          <div className="spacer">&nbsp;</div>
+          <div className="spacer">&nbsp;</div>
+          <div className="spacer">&nbsp;</div>
         </ul>
       </div>
     )
@@ -63,6 +62,14 @@ class EventIndex extends React.Component{
   render() {
     return(
       <div className="event-page-container">
+        <div id="event-public-title" className="sb">
+          Your Events:           
+          <div id="index-self-button-container">
+            <Link to="events/create">
+              <button id="index-self-create">Create New Event</button>
+            </Link>
+          </div>
+        </div>
         <div id="event-self-index-container">
           {this.renderEventBar()}
         </div>
