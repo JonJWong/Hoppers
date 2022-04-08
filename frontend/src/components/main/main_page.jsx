6723cf143abe87fdb98a35724d0fac5fe87416c0
Nick Pietrow <njpietrow@gmail.com>
2022-04-08 08:56:30 -0700

    splash changes

--- frontend/src/components/main/main_page.jsx
+++ frontend/src/components/main/main_page.jsx
@@ -11,30 +11,28 @@ class Splash extends React.Component {
           <div id="splash-contents">
             Whether it be a great night out with friends, or a journey to meet new people, Hopper has you covered!
             <div id="splash-subcontent">
-              Our app allows you to create new events, plan out a route and timeframe (itinerary), and invite friends along the way!
-              You can also make the event public, in which other users can see the event and choose to tag along if the route interests them.
-              Participants can also comment and participate in the selection of points of interest along the way, making the experience much more whole and welcoming to all!
+              With Hopper you can create new events, plan out a route and timeframe (itinerary), and invite friends along the way! <br />
+              Chat  <br />
+              Don't forget to participate in the selection of points of interest along the way, making the experience much more whole and welcoming to all!
             </div>
           </div>
-
-          {this.props.loggedIn 
-            ? (
-              <div id="splash-footer">
-                <Link
-                  to="/events">
-                    <button id="splash-entry-button">Back To Events</button>
-                </Link>
-                Please note all features subject to changes. Hoppers can not guarantee the perfect night out. Also if you have a substance abuse problem please seek assistance by dialing a the SAMHSA hotline. 1-800-662-HELP
-              </div>
+          <div id="splash-footer">
+            {this.props.loggedIn ? 
+            (
+              <Link
+                to="/events">
+                  <button id="splash-entry-button">View Events</button>
+              </Link>
             ) : (
-              <div id="splash-footer">
-                <Link
-                  to="/login">
-                  <button id="splash-entry-button">Try Hoppers Today!</button>
-                </Link>
-                Please note all features subject to changes. Hoppers can not guarantee the perfect night out. Also if you have a substance abuse problem please seek assistance by dialing a the SAMHSA hotline. 1-800-662-HELP
-              </div>
+              <Link
+                to="/login">
+                <button id="splash-entry-button">Try Hoppers Today!</button>
+              </Link>
+
             )}
+            Please note all features subject to changes. Hoppers can not guarantee the perfect night out. Also if you have a substance abuse problem please seek assistance by dialing a the SAMHSA hotline. 1-800-662-HELP
+          </div>
+          <div id="splash-block"></div>
         </div>
           
         <div id="splash-block"></div>
