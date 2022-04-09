d413214771e17c122bc9a81a87cb0f9ff2a468d8
Kevin Chen <kevinch28@gmail.com>
2022-04-09 10:01:41 -0400

    add footer links

---
+++ frontend/src/components/footer/footer.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+const Footer = () => (
+  <div className="footer">
+
+    <a href="https://github.com/JonJWong/Hoppers" target="_blank" rel="noopener noreferrer">
+      <i className="fab fa-github fa-3x"></i>
+    </a>
+
+    <a href="https://github.com/JonJWong/" target="_blank" rel="noopener noreferrer">
+      <div className="footer-image">
+        <img src="https://avatars.githubusercontent.com/u/96433015?v=4" alt="jon" width="50" height="50" />
+      </div>
+    </a>
+
+    <a href="https://github.com/njpietrow" target="_blank" rel="noopener noreferrer">
+      <div className="footer-image">
+        <img src="https://avatars.githubusercontent.com/u/25106777?v=4" alt="nick" className="footer-image" width="50" height="50" />
+      </div>
+    </a>
+
+    <a href="https://github.com/CodyDegraffeNiles" target="_blank" rel="noopener noreferrer">
+      <div className="footer-image">
+        <img src="https://avatars.githubusercontent.com/u/79245580?v=4" alt="cody" className="footer-image" width="50" height="50" />
+      </div>
+    </a>
+
+    <a href="https://github.com/KevinCh28" target="_blank" rel="noopener noreferrer">
+      <div className="footer-image">
+        <img src="https://avatars.githubusercontent.com/u/61332776?v=4" alt="kevin" className="footer-image" width="50" height="50" />
+      </div>
+    </a>
+
+  </div>
+)
+
+export default Footer;
